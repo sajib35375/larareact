29c8599bc2e14c29b1f1e7c13085bf61b1b6b23a
shahnewaj sajib <you@example.com>
2023-11-27 04:38:05 +0600

    setup project

---
+++ src/components/TopBanner/Banner.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import '../../assets/css/style.css';
+
+const Banner = () => {
+  return (
+    <>
+    <div id='topBanner'>
+        <div className="row">
+            <div className="col-md-12">
+               
+            </div>
+        </div>
+    </div>
+    
+    </>
+  )
+}
+
+export default Banner
